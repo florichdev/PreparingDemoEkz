f47b56d01924778e2b1a5b193ba434b891f86c27
florichdev <furut4yo@gmail.com>
2025-09-15 12:30:45 +0300

    Простой проект

---
+++ PartnersApp/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using PartnersApp.Models;
+using PartnersApp.Services;
+using System.ComponentModel;
+using System.Windows;
+
+namespace PartnersApp
+{
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private Service _dataService = new Service();
+        private Partner _selectedPartner;
+
+        public bool IsAdmin { get; }
+
+        public MainWindow(bool isAdmin)
+        {
+            InitializeComponent();
+            IsAdmin = isAdmin;
+            DataContext = this;
+            LoadData();
+        }
+
+        private void LoadData() => PartnersGrid.ItemsSource = _dataService.GetPartners();
+
+        private void AddPartner_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsAdmin && new PartnerWindow().ShowDialog() == true)
+                LoadData();
+        }
+
+        private void EditPartner_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsAdmin && _selectedPartner != null && new PartnerWindow(_selectedPartner).ShowDialog() == true)
+                LoadData();
+        }
+
+        private void DeletePartner_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsAdmin && _selectedPartner != null &&
+                MessageBox.Show("Удалить партнера?", "Подтверждение", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                _dataService.DeletePartner(_selectedPartner.Id);
+                LoadData();
+            }
+        }
+
+        private void ShowSales_Click(object sender, RoutedEventArgs e)
+        {
+            if (_selectedPartner != null)
+            {
+                new SalesWindow(_selectedPartner.Id, IsAdmin).ShowDialog();
+                LoadData();
+            }
+        }
+
+        private void PartnersGrid_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+            => _selectedPartner = PartnersGrid.SelectedItem as Partner;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
---
+++ PartnersApp/Services/Service.cs
@@ -0,0 +1,124 @@
+﻿using PartnersApp.dbContext;
+using PartnersApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PartnersApp.Services
+{
+    public class Service : IDisposable
+    {
+        private PartnersDBEntities _db = new PartnersDBEntities();
+
+        public bool Authenticate(string login, string password, out bool isAdmin)
+        {
+            var user = _db.Users.FirstOrDefault(u => u.Login == login && u.Password == password);
+            isAdmin = user?.IsAdmin ?? false;
+            return user != null;
+        }
+
+        public List<Partner> GetPartners() => _db.Partners.Select(p => new Partner
+        {
+            Id = p.PartnerID,
+            Name = p.PartnerName,
+            TypeId = p.PartnerTypeID,
+            TypeName = p.PartnerTypes.TypeName,
+            Director = p.DirectorName,
+            Phone = p.Phone,
+            Email = p.Email,
+            Address = p.Address,
+            Rating = p.Rating
+        }).ToList();
+
+        public void SavePartner(Partner partner)
+        {
+            var entity = partner.Id == 0 ? new Partners() : _db.Partners.Find(partner.Id);
+            if (partner.Id == 0) _db.Partners.Add(entity);
+
+            entity.PartnerName = partner.Name;
+            entity.PartnerTypeID = partner.TypeId;
+            entity.DirectorName = partner.Director;
+            entity.Phone = partner.Phone;
+            entity.Email = partner.Email;
+            entity.Address = partner.Address;
+            entity.Rating = partner.Rating;
+
+            _db.SaveChanges();
+        }
+
+        public void DeletePartner(int id)
+        {
+            var partner = _db.Partners.Find(id);
+            if (partner != null) _db.Partners.Remove(partner);
+            _db.SaveChanges();
+        }
+
+        public List<Product> GetProducts() => _db.Products.Select(p => new Product
+        {
+            Id = p.ProductID,
+            Name = p.ProductName,
+            Price = p.Price
+        }).ToList();
+
+        public void SaveProduct(Product product)
+        {
+            var entity = product.Id == 0 ? new Products() : _db.Products.Find(product.Id);
+            if (product.Id == 0) _db.Products.Add(entity);
+
+            entity.ProductName = product.Name;
+            entity.Price = product.Price;
+
+            _db.SaveChanges();
+        }
+
+        public void DeleteProduct(int id)
+        {
+            var product = _db.Products.Find(id);
+            if (product != null) _db.Products.Remove(product);
+            _db.SaveChanges();
+        }
+
+        public List<Sale> GetSales(int partnerId) => _db.SalesHistory
+            .Where(sh => sh.PartnerID == partnerId)
+            .Select(sh => new Sale
+            {
+                Id = sh.SaleID,
+                PartnerId = sh.PartnerID,
+                ProductId = sh.ProductID,
+                ProductName = sh.Products.ProductName,
+                Date = sh.SaleDate,
+                Quantity = sh.Quantity,
+                Total = sh.Quantity * sh.Products.Price
+            }).ToList();
+
+        public void AddSale(int partnerId, int productId, int quantity)
+        {
+            _db.SalesHistory.Add(new SalesHistory
+            {
+                PartnerID = partnerId,
+                ProductID = productId,
+                Quantity = quantity,
+                SaleDate = DateTime.Now
+            });
+            _db.SaveChanges();
+        }
+
+        public decimal GetTotalSales(int partnerId)
+        {
+            var result = _db.SalesHistory
+                .Where(sh => sh.PartnerID == partnerId)
+                .Sum(sh => (decimal?)sh.Quantity * sh.Products.Price);
+            return result ?? 0;
+        }
+
+        public int CalculateDiscount(decimal totalSales)
+        {
+            if (totalSales < 10000) return 0;
+            if (totalSales < 50000) return 5;
+            if (totalSales < 300000) return 10;
+            return 15;
+        }
+
+        public void Dispose() => _db?.Dispose();
+    }
+}
