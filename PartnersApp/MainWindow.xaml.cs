9070ac2affde29aeb3f541cadb4903c98fbd6d0d
florichdev <furut4yo@gmail.com>
2025-09-22 12:58:39 +0300

    Модуль авторизации

---
+++ PartnersApp/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using PartnersApp.Models;
+using PartnersApp.Services;
+using System.ComponentModel;
+using System.Windows;
+
+namespace PartnersApp
+{
+    public partial class MainWindow : Window
+    {
+        public bool IsAdmin { get; }
+
+        public MainWindow(bool isAdmin)
+        {
+            InitializeComponent();
+            IsAdmin = isAdmin;
+            DataContext = this;
+        }
+    }
+}
